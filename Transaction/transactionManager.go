0e4036cefbda7cf1f62f18ecbee1e2594df82d57
guilherme gomes rocha <guilherme.rocha@bb.com.br>
2019-01-16 12:23:46 -0200

    acerto no json de request

--- Transaction/transactionManager.go
+++ Transaction/transactionManager.go
@@ -8,7 +8,6 @@ import (
 	pending "kernel-concierge/Pending"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/satori/go.uuid"
@@ -23,16 +22,13 @@ type ResponseMessage struct {
 type RequestData struct {
 	IDRequest    string `json:"idRequest"`
 	JaggerParams string `json:"jagerParams"`
-	// Accounts     struct {
-	// 	AccountsOperations []struct {
-	// 		ID    string `json:"id"`
-	// 		Value int    `json:"value"`
-	// 	} `json:"accountsOperations"`
-	// }
-	AccountsOperations string
+	Accounts     struct {
+		AccountsOperations []struct {
+			ID    string `json:"id"`
+			Value int    `json:"value"`
+		} `json:"accountsOperations"`
+	} `json:"accounts"`
 }
-
-// const letterBytes = "ab"
 
 var rm ResponseMessage
 
@@ -42,30 +38,21 @@ func Manager(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	rm = ResponseMessage{}
 
-	// rand.Seed(time.Now().UnixNano())
-
 	// rest boilerplate
 	rd := RequestData{}
 	req, _ := ioutil.ReadAll(r.Body)
 
-	rd.AccountsOperations = strings.TrimSpace(string(req))
+	_ = json.Unmarshal(req, &rd.Accounts)
 	rd.IDRequest = uuid.Must(uuid.NewV4()).String()
+	rd.JaggerParams = ""
 
-	// if err != nil {
-	// 	log.Println(debuggin.Tracer(), "invalid account operations", err)
-	// 	rm.Msg = "Invalid account operations format"
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	resp, _ := json.Marshal(rm)
-	// 	w.Write(resp)
-	// 	return
-	// }
+	jsonRequest, _ := json.Marshal(rd)
 
 	// creates pending request to add do the stream pending map
 	pr := pending.Request{}
-	// pr.RequestID = string(letterBytes[rand.Intn(len(letterBytes))])
 	pr.RequestID = rd.IDRequest
 	pr.ResponseChan = make(chan string)
-	pr.RequestData = rd.AccountsOperations
+	pr.RequestData = string(jsonRequest)
 	// defers closing the channel and any other resource opened
 	defer closeResources(pr)
 
