b6ff167e7caf856c8eef484dd421d5e728cbcc14
guilherme gomes rocha <guilherme.rocha@bb.com.br>
2019-01-30 12:00:04 -0200

    adds tests for env vars on checkIsUp.sh

--- checkIsUp.sh
+++ checkIsUp.sh
@@ -2,6 +2,16 @@
 
 #Check if server is up
 
+if [[ -z "${KAFKA_HOST}" ]]; then
+  echo ERRO: KAFKA_HOST não informado
+  exit 1
+fi
+
+if [[ -z "${KAFKA_PORT}" ]]; then
+  echo ERRO: KAFKA_PORT não informado
+  exit 1
+fi
+
 while [ "$(nc -z $KAFKA_HOST $KAFKA_PORT </dev/null; echo $?)" !=  "0" ];
 do sleep 5;
 echo "Waiting for KAFKA SERVER is UP and RESPONDING";
